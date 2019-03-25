744d0578c48f7807599213124ee9878c2c2dd039
Brian Edgar <bwedgar@gmail.com>
2019-03-25 22:26:24 +1300

    Create geolocation.js

---
+++ geolocation.js
@@ -0,0 +1,16 @@
+// if (navigator.geolocation) {
+//   navigator.geolocation.getCurrentPosition((position) => {
+//     coords = position.coords;
+//     console.log(`AT GEOLOCATION latitude: ${coords.latitude} longitude: ${coords.longitude}`)
+//     astromath.longitude = astromath.radians(coords.longitude); //longitude of observer in degrees East
+//     astromath.latitude = astromath.radians(coords.latitude); //latitude of observer in degrees
+//     console.log(`astromath:  latitude: ${astromath.latitude} longitude: ${astromath.longitude}`)
+//     // var celestialObjects = new CelestialObjects;
+//     // var cos = celestialObjects.makeCelestialObjects();
+//     // let celestialObjects = new CelestialObjects;
+//     // var cos = []
+//     cos = celestialObjects.makeCelestialObjects();
+//     alert(cos[1].name)
+//     showData(1);
+//   });
+// }
