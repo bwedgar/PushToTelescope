a0be291858a64c54c2156132b0e7703313adb05c
Brian Edgar <bwedgar@gmail.com>
2020-01-25 07:37:57 +1300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,43 +1,78 @@
-console.log("service worker available?  " + ('serviceWorker' in navigator));
-if ('serviceWorker' in navigator) {
-    console.log("before add listener");
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('sw.js').then((registration) => {
-            console.log("Service Worker registration successful: ", registration)
-        },
-      (err) => {
-            console.log("Registration failed", err)
-        })
-    })
-}
-    else { console.log("service worker not available") }
+/*
+ Copyright 2016 Google Inc. All Rights Reserved.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
-console.log("after service worker registration attempt");
+// Names of the two caches used in this version of the service worker.
+// Change to v2, etc. when you update any of the local resources, which will
+// in turn trigger the install event again.
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
 
-let cache_name = 'cache 1.2'
-
-let urls_to_cache = [
-    '/PushToTelescope/',
+// A list of local resources we always want to be cached.
+const PRECACHE_URLS = [
+  '/PushToTelescope/',
     '/PushToTelescope/index.html',
     '/PushToTelescope/astromath.js',
     '/PushToTelescope/celestialobjects.js',
     '/PushToTelescope/planets.js',
     '/PushToTelescope/launch3.png',
     '/PushToTelescope/manifest.json'
-    ]
+];
 
-self.addEventListener('install', (e) => {
- e.waitUntil(caches.open(cache_name).then((cache) => {
-console.log("caching")
-  return cache.addAll(urls_to_cache)
- }) )
-})
+// The install handler takes care of precaching the resources we always need.
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
+  );
+});
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then((response) => {
-     if(response)
-      return response
-     else
-      return fetch(e.request)
-    }) )
-})
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', event => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
+  );
+});
+
+// The fetch handler serves responses for same-origin resources from a cache.
+// If no response is found, it populates the runtime cache with the response
+// from the network before returning it to the page.
+self.addEventListener('fetch', event => {
+  // Skip cross-origin requests, like those for Google Analytics.
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            // Put a copy of the response in the runtime cache.
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
+});
+
